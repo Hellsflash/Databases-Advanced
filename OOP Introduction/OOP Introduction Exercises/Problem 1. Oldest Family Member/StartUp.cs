e694a93eccca77303e4f162f1ed03d2d9ea292a9
Mario Slavov <hellsflash@gmail.com>
2017-11-04 03:27:05 +0200

    Solved All problems from OOP Introduction Exercises

---
+++ OOP Introduction/OOP Introduction Exercises/Problem 1. Oldest Family Member/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        MethodInfo oldestMemberMethod = typeof(Family).GetMethod("GetOldestMember");
+        MethodInfo addMemberMethod = typeof(Family).GetMethod("AddMember");
+        if (oldestMemberMethod == null || addMemberMethod == null)
+        {
+            throw new Exception();
+        }
+
+        var number = int.Parse(Console.ReadLine());
+        var family = new Family();
+        for (int i = 0; i < number; i++)
+        {
+            var personArgs = Console.ReadLine().Split();
+            var person = new Person(personArgs[0], int.Parse(personArgs[1]));
+
+            family.AddMember(person);
+        }
+
+        var oldestMember = family.GetOldestMember();
+
+        Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+    }
+}
---
+++ OOP Introduction/OOP Introduction Exercises/Problem 5. Football Team Generator/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var command = Console.ReadLine();
+        List<Team> teams = new List<Team>();
+
+        while (command != "END")
+        {
+            var args = command.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            switch (args[0])
+            {
+                case "Team":
+                    teams.Add(new Team(args[1]));
+                    break;
+
+                case "Add":
+                    var teamName = args[1];
+                    var playerName = args[2];
+                    var endurance = int.Parse(args[3]);
+                    var sprint = int.Parse(args[4]);
+                    var dribble = int.Parse(args[5]);
+                    var passing = int.Parse(args[6]);
+                    var shooting = int.Parse(args[7]);
+
+                    bool teamExists = teams.Any(t => t.Name == teamName);
+                    Team team;
+                    if (!teamExists)
+                    {
+                        Console.WriteLine($"Team {teamName} does not exist.");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            team = teams.First(t => t.Name == teamName);
+                            var player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
+                            team.AddPlayer(player);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    break;
+
+                case "Remove":
+
+                    teamName = args[1];
+                    team = teams.First(t => t.Name == teamName);
+                    playerName = args[2];
+
+                    try
+                    {
+                        team.RemovePlayer(playerName);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+
+                case "Rating":
+                    teamName = args[1];
+                    teamExists = teams.Any(t => t.Name == teamName);
+
+                    if (!teamExists)
+                    {
+                        Console.WriteLine($"Team {teamName} does not exist.");
+                    }
+                    else
+                    {
+                        team = teams.First(t => t.Name == teamName);
+                        Console.WriteLine($"{team.Name} - {team.Rating()}");
+                    }
+                    break;
+            }
+
+            command = Console.ReadLine();
+        }
+    }
+}
